b7dae6f5a0d1a54f9653d925da0ca192022144ef
Wyatt Huskey <whuskey6@gmail.com>
2020-10-12 13:34:26 -0600

    Completed 2.1

--- part2/src/components/Course.js
+++ part2/src/components/Course.js
@@ -2,15 +2,25 @@ import React from 'react'
 
 const Course = ({course}) => {
     const courseTitle = course.name
+    const courseParts = course.parts
+    console.log(course.parts)
     return(
         <div>
             <h3>{course.name}</h3>
+            <ul>
+                {courseParts.map(part => 
+                    <Part key={part.id} part={part} />
+                )}
+            </ul>
         </div>
     )
 }
 
-const Part = () => {
-
+const Part = ({part}) => {
+    console.log({part})
+    return(
+        <li>{part.name} {part.exercises}</li>
+    )
 }
 
 export default Course
