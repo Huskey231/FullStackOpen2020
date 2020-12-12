d44ba7658ff00b46c2cb271e9d6b8a54a8d8510e
Wyatt Huskey <whuskey6@gmail.com>
2020-12-12 14:07:19 -0700

    Completed 2.4

--- part2/src/components/Course.js
+++ part2/src/components/Course.js
@@ -1,30 +1,38 @@
 import React from 'react'
 
-const Course = ({course}) => {
-    const courseParts = course.parts
-    const totalExercises = courseParts.reduce(function(sum, coursePart) {
-        console.log(sum)
-        return sum + coursePart.exercises
-    }, 0)
-    console.log(totalExercises)
+const Course = ({courses}) => {
 
     return(
         <div>
-            <h3>{course.name}</h3>
-            <ul>
-                {courseParts.map(part => 
-                    <Part key={part.id} part={part} />
-                )}
-            </ul>
-            <b>Total of {totalExercises} excersises</b>
+            {courses.map(course =>
+                <CourseRender key={course.id} course={course} />
+            )}
         </div>
     )
 }
 
 const Part = ({part}) => {
-    console.log({part})
     return(
         <li>{part.name} {part.exercises}</li>
+    )
+}
+
+const CourseRender = ({course}) => {
+    const totalExercises = course.parts.reduce(function(sum, part) {
+        console.log(part.exercises)
+        return sum + part.exercises
+    }, 0)
+    
+    return(
+        <div>
+            <h3>{course.name}</h3>
+            <ul>
+                {course.parts.map(part =>
+                    <Part key={part.id} part={part} />
+                )}
+            </ul>
+            <b>Total of {totalExercises} excersises</b>
+        </div>
     )
 }
 
